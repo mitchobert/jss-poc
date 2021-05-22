166edc77f14f2c9ea3eb7803a384976538bf5b8a
Mitch Obert <mitchobert@Mitchs-Air.home>
2021-05-22 09:06:56 -0400

    Styling the button component and adding colors to the theme

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,38 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { createUseStyles } from 'react-jss';
 
 const useStylesFromThemeFunction = createUseStyles(theme => ({
     button: {
-        background: theme.color.green,
+        background: theme.color.blue400,
         paddingTop: theme.spacing.sm,
         paddingBottom: theme.spacing.sm,
         paddingLeft: theme.spacing.md,
         paddingRight: theme.spacing.md,
+        border: `1px solid ${theme.color.blue400}`,
         borderRadius: theme.borderRadius.sm,
-        color: theme.color.black,
-        borderColor: theme.color.green,
+        textTransform: 'uppercase',
+        color: theme.color.white,
         fontFamily: theme.fontFamily.roboto,
+        cursor: 'pointer',
+        '&:hover': {
+            background: theme.color.blue800,
+            border: `1px solid ${theme.color.blue800}`,
+        },
+        '&:focus': {
+            outline: `2px solid ${theme.color.blue900}`,
+            outlineStyle: 'auto',
+        }
     },
     label: {
         fontWeight: theme.fontWeight.medium,
     }
-}))
+}
+));
 
-function Button({ label, ...props } = this.props) {
+function Button({ className, label, ...props } = this.props) {
     const classes = useStylesFromThemeFunction(props);
     return (
-        <button className={classes.button}>
+        <button className={classNames( 'button', className, classes.button)}>
             <span className={classes.label}>{label}</span>
         </button>
     );
 }
 
 Button.defaultProps = {
+    className: '',
     label: '',
-  };
+};
 
 Button.propTypes = {
+    className: PropTypes.string,
     label: PropTypes.string,
 };
 
